0afcbddca88e8e78517b3f88402b5c5e7e1dfae2
ChenggongWang <c.wang@princeton.edu>
2022-05-27 17:23:34 -0400

    animation show u and h in same plot

--- plot_func.py
+++ plot_func.py
@@ -2,6 +2,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
+
+def my_anima_1d_h_u(fig,axs,x,t,h,u,xy_lim=None):
+    # fig: figure 
+    # x,t: x-axis annd t-axis
+    # u: output[x,t]. dimension: x.size x t.size
+    # First set up the figure, the axis, and the plot element we want to animate
+    if xy_lim is None:
+        axs[0].set_xlim([np.min(x),np.max(x)])
+        axs[0].set_ylim([np.min(h),np.max(h)*1.1])
+        axs[1].set_xlim([np.min(x),np.max(x)])
+        axs[1].set_ylim([np.min(u),np.max(u)*1.1])
+    else:
+        axs[0].set_xlim([xy_lim[0][0],xy_lim[0][1]])
+        axs[0].set_ylim([xy_lim[1][0],xy_lim[1][1]]) # h
+        axs[1].set_xlim([xy_lim[0][0],xy_lim[0][1]]) 
+        axs[1].set_ylim([xy_lim[2][0],xy_lim[2][1]]) # u
+        
+    line_h,= axs[0].plot([],[],linewidth=2)
+    line_u,= axs[1].plot([],[],linewidth=2)
+    
+    # animation function. This is called sequentially
+    def drawframe(i):
+        line_h.set_data(x,h[i])
+        line_u.set_data(x,u[i])
+        fig.suptitle(f"t={t[i]:4.2f}")
+        return line_h,line_u
+        
+    # call the animator. blit=True means only re-draw the parts that have changed.
+    anim = animation.FuncAnimation(fig, drawframe, frames=t.shape[0], interval=200, blit=True)
+    plt.close()
+    return anim
 # function for animation
 def anima_1d(fig,x,t,u,xy_lim=None):
     # fig: figure 
@@ -22,13 +53,12 @@ def anima_1d(fig,x,t,u,xy_lim=None):
         line1d.set_data(x,u[0])
         return line1d,
     # animation function. This is called sequentially
-    def animate(i,ax1,x,t,u):
+    def drawframe(i,ax1):
         line1d.set_data(x,u[i])
         fig.suptitle(f"t={t[i]:4.2f}")
         return line1d,
         
     # call the animator. blit=True means only re-draw the parts that have changed.
-    anim = animation.FuncAnimation(fig, animate,fargs=(ax1,x,t,u),init_func=init_f,
-                                   frames=t.shape[0], interval=200, blit=True)
+    anim = animation.FuncAnimation(fig, drawframe, frames=t.shape[0], interval=200, blit=True)
     plt.close()
     return anim
