7edf226bfc4358851a4409be7f3a048b502de53e
ChenggongWang <c.wang@princeton.edu>
2022-07-07 22:52:08 -0400

    Update create_pytorch_env.sh

--- create_pytorch_env.sh
+++ create_pytorch_env.sh
@@ -16,7 +16,7 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # command starts here
-conda create -y --prefix pytorch_env python=3.10 numpy=1.21
+conda create -y -n pytorch_env python=3.10 numpy=1.21
 conda activate pytorch_env
 conda install -y pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
 conda install -y -c conda-forge matplotlib=3 cartopy scipy scikit-learn numba ipykernel
